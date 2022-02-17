e8db1e53544ee5b5882fe96f3d977a7cccb9b888
Yoshi0207 <yoaplid002@hotmail.co.jp>
2022-02-17 22:37:42 +0900

    feat: ローカルサーバを起動中にsrcを修正したときにオートトランスパイルするように修正

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-
-console.error('bbbb')
+console.log("hello typescript")
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,21 @@
 const path = require('path');
 module.exports = {
-    entry: {
-        bundle: './src/index.ts'
-    },  
-    output: {
-        path: path.join(__dirname,'dist'),
-        publicPath: './example',
-        filename: '[name].js'
-    },
-    resolve: {
-        extensions:['.ts','.js']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "example"),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test:/\.ts$/,loader:'ts-loader'
-            }
-        ]
-    }
+  entry: {
+    'photo-collage.bundle': './src/index.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, loader: 'ts-loader'
+      }
+    ]
+  }
 }
